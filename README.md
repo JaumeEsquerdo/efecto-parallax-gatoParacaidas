11c82be94fd0173d4cd5e9060c0dbccadba6bdab
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-04-30 19:48:58 +0200

    info scrollYprogress

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ Voy a utilizar framer-motion con vite para hacer este efecto y practicarlo.
 
 - motion: Componente que hace que los elementos sean animados en función de valores dinámicos (como el scroll).
 
+### Descripción de uso
+Parte	Descripción
+- `scrollYProgress`	Progreso del scroll (de 0 a 1)
+- `[0, 1]`	Rango del progreso del scroll, desde el inicio (0) hasta el final (1)
+- `['-40%', '40%']`	El rango de transformación en el eje Y para la propiedad y del gato, de -40% (arriba) a 40% (abajo)
+Resultado: El gato se mueve desde el 40% por encima de su posición inicial hasta el 40% por debajo de su posición inicial conforme el usuario hace scroll.
--- gato-paracaidas-parallax/src/pages/Home.jsx
+++ gato-paracaidas-parallax/src/pages/Home.jsx
@@ -1,7 +1,84 @@
+import { motion, useScroll, useTransform} from 'framer-motion'
+import { useRef } from 'react';
+import cielo from '../assets/imgs/cielo.jpg';
+import jardin from '../assets/imgs/jardin.jpg';
+import gato from '../assets/imgs/cat.gif';
+
+
 const Home = () => {
+    const containerRef = useRef(null)
+    const {scrollYProgress} = useScroll({
+        target: containerRef,
+        offset: ['start start', 'end end'] // la ref start empieza a contar el scroll en el start y llega a 1 cuando toca el final del viewport
+
+    });
+    
+
+    // movimiento lento para el cielo
+    const cieloY = useTransform(scrollYProgress, [0,1], ['0%', '20%']);
+    // movimiento medio para el jardin (mas aproximado que el cielo)
+    const jardinY = useTransform(scrollYProgress, [0,1], ['100%', '40%'])
+    // movimiento del gato (arriba a abajo)
+    const gatoY = useTransform(scrollYProgress, [0,1], ['-80%', '80%'])
+    const cieloOpacity = useTransform(scrollYProgress, [0, 1], [1, 0.5]); // Cielo menos visible mientras avanzas
+
+
     return (
         <>
-        Gato
+        <div ref={containerRef} style={{height:'130vh', position:'relative', overflow:'hidden'}}>
+        <motion.img 
+            src={cielo}
+            alt='cielo'
+            style={{
+                position: 'absolute',
+                top:0,
+                left:0,
+                width:'100%',
+                height:'100vh',
+                objectFit:'cover',
+                y: cieloY,
+                zIndex:1,
+                opacity: cieloOpacity,
+            }}
+            
+            
+            />
+            <motion.img 
+            src={jardin}
+            alt='jardin'
+            style={{
+                position: 'absolute',
+                top:0,
+                left:0,
+                width:'100%',
+                height:'100vh',
+                objectFit:'cover',
+                y: jardinY,
+                zIndex:2
+            }}
+            />
+            
+            <motion.img 
+            src={gato}
+            alt='gato'
+            style={{
+                position: 'absolute',
+                top:'35%',
+                left:'20%',
+                transform:'translateX(-50%)',
+                width:'250px',
+                y: gatoY,
+                zIndex:3
+            }}
+            transition={{ type: 'spring', stiffness: 50, damping: 20 }}
+
+            
+            
+            />
+            
+            
+        </div>
+        
         </>
     );
 }
